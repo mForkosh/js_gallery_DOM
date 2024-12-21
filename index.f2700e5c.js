dcbef58f6300706a88dc76a2fe7c1de62b3b9b36
Mykhailo Forkosh <forkos277@gmail.com>
2024-12-21 17:12:10 +0200

    make build

---
+++ index.f2700e5c.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.querySelector(".gallery__large-img");e.addEventListener("click",function(e){var r=e.target.closest(".list-item__link");r&&(t.src=r.href),e.preventDefault()});
+//# sourceMappingURL=index.f2700e5c.js.map
